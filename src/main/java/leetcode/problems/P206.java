bc61c75f74c9e38b733830a6453bdb725ef68152
2071283913 <2071283913@qq.com>
2019-11-19 13:06:20 +0800

    P206 - iteratively O(n) solution

---
+++ src/main/java/leetcode/problems/P206.java
@@ -0,0 +1,27 @@
+package leetcode.problems;
+
+public class P206 {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
