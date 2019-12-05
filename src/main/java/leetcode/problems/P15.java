9a8ea0709ac70214cd4988dd5b595246d8112e35
2071283913 <2071283913@qq.com>
2019-12-05 13:01:38 +0800

    P15 - sort and two pointer O(n^2) solution

--- src/main/java/leetcode/problems/P15.java
+++ src/main/java/leetcode/problems/P15.java
@@ -1,7 +1,8 @@
 package leetcode.problems;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class P15 {
 
@@ -9,48 +10,46 @@ public class P15 {
         if (nums == null || nums.length < 3)
             return new ArrayList<>(0);
 
-        List<int[]> solutions = new ArrayList<>();
+        Arrays.sort(nums);
+
+        List<List<Integer>> solutions = new ArrayList<>();
 
         int length = nums.length;
 
         for (int ai = 0; ai < length - 2; ++ai) {
             int a = nums[ai];
+            if (a > 0) break;
+            if (ai > 0 && nums[ai] == nums[ai-1]) continue;
 
-            Set<Integer> visited = new HashSet<>(length - 2);
-            for (int bi = ai + 1; bi < length; ++bi) {
-                int b = nums[bi];
-                int c = 0 - a - b;
-                if (visited.contains(c)) {
-                    if (! contains(solutions, a, c, b)) {
-                        solutions.add(new int[] {a, c, b});
-                    }
+            int lo = ai + 1;
+            int hi = length - 1;
+            while (lo < hi) {
+                if (lo > ai + 1 && nums[lo] == nums[lo - 1]) {
+                    lo++;
+                }
+                else if (hi < length - 1 && nums[hi] == nums[hi + 1]) {
+                    hi--;
                 }
                 else {
-                    visited.add(b);
+                    int b = nums[lo];
+                    int c = nums[hi];
+
+                    int sum = a + b + c;
+                    if (sum == 0) {
+                        solutions.add(Arrays.asList(a, b, c));
+                        lo++;
+                        hi--;
+                    }
+                    else if (sum < 0) {
+                        lo++;
+                    }
+                    else {
+                        hi--;
+                    }
                 }
             }
         }
 
-        return solutions.stream().map(
-                arr -> Arrays.stream(arr).boxed().collect(Collectors.toList())
-        ).collect(Collectors.toList());
-    }
-
-    private static boolean contains(List<int[]> solutions, int... target) {
-        if (solutions.isEmpty()) return false;
-
-        for (int[] c : solutions) {
-            if (equals(target, c)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private static boolean equals(int[] va, int[] vb) {
-        Arrays.sort(va);
-        Arrays.sort(vb);
-        return Arrays.equals(va, vb);
+        return solutions;
     }
 }
