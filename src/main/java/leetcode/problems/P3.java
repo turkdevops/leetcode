6003bb90d62761703587a87db09d579e0dafa056
2071283913 <2071283913@qq.com>
2019-12-11 13:33:35 +0800

    P3 - one-pass O(n) solution

--- src/main/java/leetcode/problems/P3.java
+++ src/main/java/leetcode/problems/P3.java
@@ -1,35 +1,37 @@
 package leetcode.problems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class P3 {
 
     public int lengthOfLongestSubstring(String s) {
         if (s == null || s.isEmpty()) return 0;
 
-        int longest = 1;
-
         int length = s.length();
-        for (int i = 0; i < length - 1; ++i) {
-            for (int j = i + 1; j < length; ++j) {
-                if (!isUnique(s, i, j, s.charAt(j))) {
-                    longest = Math.max(longest, j - i);
-                    break;
+        Map<Character, Integer> indexes = new HashMap<>(length);
+
+        int longest = 0;
+        int beginIndex = 0;
+
+        for (int i = 0; i < length; ++i) {
+            Integer oldIndex = indexes.put(s.charAt(i), i);
+            if (oldIndex != null) {
+                longest = Math.max(longest, i - beginIndex);
+
+                // remove from [beginIndex, oldIndex)
+                // do not need to remove oldIndex since it is replaced with i
+                for (int bi = beginIndex; bi < oldIndex; ++bi) {
+                    indexes.remove(s.charAt(bi));
                 }
-                else if (j == length - 1) {
-                    longest = Math.max(longest, j - i + 1);
-                }
+
+                beginIndex = oldIndex + 1;
+            }
+            else if (i == length - 1) {
+                longest = Math.max(longest, i - beginIndex + 1);
             }
         }
 
         return longest;
-    }
-
-    private static boolean isUnique(String s, int from, int to, char c) {
-        for (int i = from; i < to; ++i) {
-            if (c == s.charAt(i)) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
